cb88d7d47a34eb914ee1ce5483c338d78fdc30d5
leandro.osouza <leandro.osouza@CAS554943W07I03.senacsp.edu.br>
2017-03-30 16:39:41 -0300

    Par ou Impar

---
+++ src/parouimpar/Parouimpar.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parouimpar;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author leandro.osouza
+ */
+public class Parouimpar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      int numero = 0;
+      Scanner leia = new Scanner(System.in);
+        System.err.println("Digite o Numero");
+        int resultado=leia.nextInt();
+        if(numero % 2==0 )
+        System.err.println("É Par");
+        else
+        System.err.println("É Impar");
+      
+        
+        
+            
+                
+                
+      
+    }
+    
+}
